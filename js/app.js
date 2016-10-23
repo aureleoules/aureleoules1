d29e8d47f9b0f1e5970543eed97f87c51ed7d5e5
aureleoules <aurele@oules.com>
2016-10-23 19:24:18 +0200

    Head Title change when ng-route change

--- js/app.js
+++ js/app.js
@@ -2,27 +2,38 @@ var app = angular.module('website', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider.
     when('/about', {
+        title: "About",
         templateUrl: 'partials/about.html'
     }).
     when('/blog', {
         templateUrl: 'partials/blog.html'
     }).
     when('/', {
+        title: "Home",
         redirectTo: '/home',
         templateUrl: 'partials/home.html'
     }).
     when('/home', {
+        title: "Home",
         redirectTo: '/home',
         templateUrl: 'partials/home.html'
     }).
+    when('/404', {
+        title: "Not Found",
+        templateUrl: 'partials/404.html'
+    }).
     otherwise({
         redirectTo: '/404',
-        templateUrl: 'partials/404.html'
     });
 
 });
 app.controller('MainCtrl', function($scope) {
-
+    $scope.titlePage = "test";
     $scope.tooltipText = "That's a joke, click to fix.";
-    $scope.texte = "<b>test</b>"
 });
+
+app.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);

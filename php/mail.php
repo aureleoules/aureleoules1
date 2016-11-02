007d7346deefdc0fe35f625aca1d8137b464782b
aureleoules <aurele@oules.com>
2016-11-02 20:23:59 +0100

    Contact Form Button get disabled when submitted + text showing if successful

--- php/mail.php
+++ php/mail.php
@@ -1,10 +1,11 @@
+<?php header('Access-Control-Allow-Origin: *'); ?>
 <?php
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $email \n Message: $message";
+$formcontent="From: $email \nMessage: $message";
 $recipient = "aurele@oules.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail("$recipient", $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . $email;
 ?>
